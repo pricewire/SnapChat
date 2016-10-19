ed9f13733366b298c49d438430e0c6d2d6ace24d
Mark <mark@comcast.net>
2016-10-19 08:51:40 -0400

    Added Snaps lecture 109

--- SnapChat/SelectUserViewController.swift
+++ SnapChat/SelectUserViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class SelectUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var users : [User] = []
     
     
     override func viewDidLoad() {
@@ -19,14 +22,42 @@ class SelectUserViewController: UIViewController, UITableViewDataSource, UITable
         self.tableView.dataSource = self
         self.tableView.delegate = self
         
+        FIRDatabase.database().reference().child("users").observe(FIRDataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+            
+            
+            let user = User()
+            user.email = (snapshot.value as! NSDictionary)["email"] as! String
+            
+            user.uid = snapshot.key
+            
+            self.users.append(user)
+            
+            self.tableView.reloadData()
+        })
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.email
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let user = users[indexPath.row]
+        
+        let snap = ["from":user.email, "description":"hello", "imageURL":"www.kite@gotoit.net"]
+        
+        FIRDatabase.database().reference().child("users").child(user.uid).child("snaps").childByAutoId().setValue(snap)
     }
 
 
---
+++ SnapChat/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SnapChat
+//
+//  Created by Mark Langlois on 10/18/16.
+//  Copyright © 2016 Mark Langlois. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var email = ""
+    var uid = ""
+}
